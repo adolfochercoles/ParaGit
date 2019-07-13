1211f85677b5f8279b52128669f346c0d07af38a
Adolfo Chércoles <adolfochercoles@gmail.com>
2019-07-13 16:27:59 -0300

    Staging area 1

--- ParaGit/ParaGit/Program.cs
+++ ParaGit/ParaGit/Program.cs
@@ -11,6 +11,7 @@ namespace ParaGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo");
+            Console.WriteLine("Adios...");
             Console.ReadLine();
         }
     }
